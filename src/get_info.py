395063b0b32e1ac072cf75da3e091d519b46b5bf
Yotam Ben Dov <yotam.bendov985@gmail.com>
2023-06-15 22:32:29 +0300

    python script prints out the JSON contents still need to figure out how to create JSON in local host

--- src/get_info.py
+++ src/get_info.py
@@ -7,32 +7,33 @@ def get_info():
     """
     config.load_kube_config() # load local config file
     v1 = client.CoreV1Api() # create api object
-    
     info = [] # list to store info
+    
     pods = v1.list_pod_for_all_namespaces(watch=False) # all pods in all namespaces
+    deployments = v1.list_deployment_for_all_namespaces(watch=False) # all deployments in all namespaces
+    
+    get_pod_info(info, pods)
     # iterate through all pods and get info
-    for pod in pods.items:
-        workload = (str(pod.metadata.name), 
-                    str(pod.kind), 
-                    str(pod.metadata.namespace), 
-                    str(pod.metadata.uid))
-        # if workload not in info, add it
-        info.append(workload)
     return info
 
-def to_json(info):
-    """
-    convert info into json format
-    """
-    return json.dumps(info)
+def get_pod_info(info, pods):
+    for pod in pods.items:
+        workload = {
+            "name": pod.metadata.name,
+            "type": "pod",
+            "namespace": pod.metadata.namespace,
+            "uid": pod.metadata.uid
+        }
+    info.append(workload)
+    
 
 def main():
     """
     Main function
     """
     info = get_info()
-    json_info = to_json(info)
-    return(json_info)
+    json_info = json.dumps(info, indent=4)
+    print(json_info)
 
 if __name__ == "__main__":
     main()
